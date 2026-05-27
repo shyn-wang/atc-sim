1781917ac561354fad5c35ecf4d16fd20beea945
David Wang <david.wang00008@gmail.com>
2026-05-26 20:02:44 -0400

    implement random arriving plane spawns & heading selection

--- src/main/java/com/example/wangatc/ArrivingPlane.java
+++ src/main/java/com/example/wangatc/ArrivingPlane.java
@@ -1,4 +1,27 @@
 package com.example.wangatc;
 
-public class ArrivingPlane {
+public class ArrivingPlane extends Plane {
+
+    public ArrivingPlane() {
+        super("airborne", 0, 0);
+
+        int[] spawnPosition = Util.generateRandomSpawnPoint();
+        this.setX(spawnPosition[0]);
+        this.setY(spawnPosition[1]);
+
+        // point plane towards center of screen
+
+        // calculate x and y components of direct line to center
+        double deltaX = ((double) Util.screenWidth / 2) - this.getX();
+        double deltaY = ((double) Util.screenHeight / 2) - this.getY();
+
+        // calculate angle of flight path to center (direction plane must travel in relative to map)
+        double initialHeading = Math.toDegrees(Math.atan2(deltaY, deltaX));
+
+        this.setCurrentHeading(initialHeading);
+        this.setTargetHeading(initialHeading);
+    }
+
+
+
 }
